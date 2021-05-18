37c06854785527bb2e7db89dbf5692ad08671564
Deivid <deivid.magno.nascimento@gmail.com>
2021-05-17 21:45:38 -0300

    Draw flag component

--- App.js
+++ App.js
@@ -20,7 +20,9 @@ export default class App extends Component {
         <Field opened nearMines={6}/>
         <Field mined/>
         <Field mined opened/>
-        <Field mined opened exploded/>
+        <Field mined opened exploded/>        
+        <Field flagged/>  
+        
       </View>
     );
   }
---
+++ src/business.js
@@ -0,0 +1,10 @@
+const createBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row)=>{
+        return Array(columns).fill(0).map((_, column)=>{
+            return {
+                row: row,
+                column: column,
+            }
+        })
+    })
+}
